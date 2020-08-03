238ac7511e7b1901826af2fb48613b57fee356eb
Mohamed ELBAKKALI <med.el.developer@gmail.com>
2020-08-03 14:19:36 +0100

    Add project files.

---
+++ ConsoleAppCoreiTextSharp/ITextManager.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using iText.Kernel.Pdf;
+using iText.Layout;
+using iText.Layout.Element;
+using iText.Layout.Properties;
+
+namespace ConsoleAppCoreiTextSharp
+{
+    class ITextManager
+    {
+        public void Write(String path,string text)
+        {
+            // Must have write permissions to the path folder 
+            String pat = @"‪Pdf_for_core\mypdf";
+            PdfWriter writer = new PdfWriter(pat);
+            PdfDocument pdf = new PdfDocument(writer);
+            Document document = new Document(pdf);
+            Paragraph header = new Paragraph(text)
+               .SetTextAlignment(TextAlignment.CENTER)
+               .SetFontSize(20);
+
+            document.Add(header);
+            document.Close();
+        }
+    }
+}
---
+++ ConsoleAppCoreiTextSharp/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using iText.Kernel.Pdf;
+using iText.Layout;
+using iText.Layout.Element;
+using iText.Layout.Properties;
+
+namespace ConsoleAppCoreiTextSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            ITextManager textManager = new ITextManager();
+            textManager.Write(@"Pdf_for_core\mypdf.pdf", "Center Header") ;
+
+            Console.WriteLine("Done!");
+        }
+    }
+}
